8020800f1352572ad239572093347b29a5a051de
kostyaLem <kostyaLem@yandex.ru>
2020-12-17 03:07:29 +0300

    login view ready

--- FuzzyLogic.UI/ViewModels/AuthViewModel.cs
+++ FuzzyLogic.UI/ViewModels/AuthViewModel.cs
@@ -3,15 +3,8 @@ using System;
 
 namespace FuzzyLogic.UI.ViewModels
 {
-    public sealed class AuthViewModel : NamedViewModel
+    public sealed class AuthViewModel
     {
-        public override string Title => "Авторизация";
-    }
 
-    public abstract class NamedViewModel : ViewModelBase
-    {
-        public abstract string Title { get; }
-
-        public virtual string Description { get; protected set; }
     }
 }
