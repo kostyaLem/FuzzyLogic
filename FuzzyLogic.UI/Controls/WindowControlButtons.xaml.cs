3a7a54d2df7b19d97b55a87abc400b83a98ee737
kostyaLem <kostyaLem@yandex.ru>
2020-12-27 18:13:50 +0300

    added control of window management

---
+++ FuzzyLogic.UI/Controls/WindowControlButtons.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FuzzyLogic.UI.Controls
+{
+    public partial class WindowControlButtons : UserControl
+    {
+        public ICommand SkipCommand { get; set; }
+        public ICommand ExpandCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+
+        public bool ShowExpanderButton
+        {
+            get { return (bool)GetValue(ShowExpanderButtonProperty); }
+            set { SetValue(ShowExpanderButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowExpanderButtonProperty =
+            DependencyProperty.Register(nameof(ShowExpanderButton), typeof(bool), typeof(WindowControlButtons), new PropertyMetadata(true));
+
+
+        public bool ShowSkipButton
+        {
+            get { return (bool)GetValue(ShowSkipButtonProperty); }
+            set { SetValue(ShowSkipButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowSkipButtonProperty =
+            DependencyProperty.Register(nameof(ShowSkipButton), typeof(bool), typeof(WindowControlButtons), new PropertyMetadata(true));
+
+
+        public bool ShowCloseButton
+        {
+            get { return (bool)GetValue(ShowCloseButtonProperty); }
+            set { SetValue(ShowCloseButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowCloseButtonProperty =
+            DependencyProperty.Register(nameof(ShowCloseButton), typeof(bool), typeof(WindowControlButtons), new PropertyMetadata(true));
+
+
+        public WindowControlButtons()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FuzzyLogic.UI/Resources/Extensions/SolidColorAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace FuzzyLogic.UI.Resources.Extensions
+{
+    public class SolidColorAnimation : ColorAnimation
+    {
+        public SolidColorBrush ToBrush
+        {
+            get { return To == null ? null : new SolidColorBrush(To.Value); }
+            set { To = value?.Color; }
+        }
+    }
+}
